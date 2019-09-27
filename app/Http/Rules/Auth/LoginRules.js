3f00516c1df1af02d2a42c4bcdaad62d1332c8f5
fahriansyah <fachryansyah123@gmail.com>
2019-09-27 16:28:43 +0700

    add email rules

--- app/Http/Rules/Auth/LoginRules.js
+++ app/Http/Rules/Auth/LoginRules.js
@@ -2,5 +2,6 @@ const { body } = require("express-validator")
 
 const LoginRules = [
     body("email", "field email can't be null").exists(),
-    body("email", "not email").isEmail()
+    body("email", "not email").isEmail(),
+    body("password", "field password can't be null").exists()
 ]
