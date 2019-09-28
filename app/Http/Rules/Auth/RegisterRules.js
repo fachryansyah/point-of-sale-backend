0e1f2426463aa76ce1cb195b934750b17158b1a2
fahriansyah <fachryansyah123@gmail.com>
2019-09-28 09:31:56 +0700

    add rules email is exist, and product already exist

--- app/Http/Rules/Auth/RegisterRules.js
+++ app/Http/Rules/Auth/RegisterRules.js
@@ -1,15 +1,23 @@
 const { body } = require("express-validator")
+const User = require("../../../Models/User")
 
 const RegisterRules = [
-    body("firstname", "field firstname can't be null").exists(),
-    body("firstname", "field firstname maximal 60 character").isLength({max:60}),
-    body("lastname", "field lastname can't be null").exists(),
-    body("lastname", "field lastname maximal 60 character").isLength({max:60}),
-    body("email", "field email can't be null").exists(),
-    body("email", "email not valid").isEmail(),
-    body("password", "field password can't be null").exists(),
-    body("password", "field password maximal 50 character").isLength({max: 50}),
-    body("password", "field password minimum 6 character").isLength({min: 6})
+    body("firstname", "field Firstname can't be null").exists(),
+    body("firstname", "field Firstname maximal 60 character").isLength({max:60}),
+    body("lastname", "field Lastname can't be null").exists(),
+    body("lastname", "field Lastname maximal 60 character").isLength({max:60}),
+    body("email", "field Email can't be null").exists(),
+    body("email", "Email not valid").isEmail(),
+    body("email", "Email already in use").custom(value => {
+        const user = User.query().findOne({
+            email: value
+        })
+
+        return (user instanceof User == false ? true : false)
+    }),
+    body("password", "field Password can't be null").exists(),
+    body("password", "field Password maximal 50 character").isLength({max: 50}),
+    body("password", "field Password minimum 6 character").isLength({min: 6})
 ]
 
 module.exports = RegisterRules;
--- app/Http/Rules/Product/CreateProductRules.js
+++ app/Http/Rules/Product/CreateProductRules.js
@@ -1,8 +1,16 @@
 const { body } = require("express-validator")
+const Product = require("../../../Models/Product")
 
 const CreateProductRules = [
     body("name", "field name cant't be null").exists(),
     body("name", "field name maximal 110 character").isLength({max: 110}),
+    body("name", "Product already exists").custom(value => {
+        const product = Product.query().findOne({
+            name: value
+        })
+
+        return (product instanceof Product == false ? true : false)
+    }),
     body("description", "field description can't be null").exists(),
     body("description", "field description maximal 1000 character").isLength({max: 1000}),
     body("category_id", "field category can't be null").exists(),
